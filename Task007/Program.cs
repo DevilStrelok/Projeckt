ab4e382c193bf41545f1a39f99251691d42c3406
Sergey_Puchnin <puchnin1996@mail.ru>
2023-03-07 12:50:46 +0300

    Добавили в Task007 задачу

--- Task007/Program.cs
+++ Task007/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход
+// трёхзначное число и на выходе показывает последнюю цифру
+// этого числа.
+// 456 -> 6
+// 782 -> 2
+// 918 -> 8
+
+Console.Write("Введите трёхзначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if(number >= 100 && number <= 999)
+{
+    int numberlast = number % 10;
+    Console.WriteLine(numberlast);
+}
+else
+{
+    Console.WriteLine("Введите ТРЁХзначное число: ");
+}
