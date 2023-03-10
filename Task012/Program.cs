47efe3b0cfa410cefd2255ac01ccd35230d26aa6
Sergey_Puchnin <puchnin1996@mail.ru>
2023-03-10 12:16:24 +0300

    Добавили в Task012 задачу

--- Task012/Program.cs
+++ Task012/Program.cs
@@ -1,2 +1,57 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая будет принимать на
+// вход два числа и выводить, является ли первое число
+// кратным второму. Если число 1 не кратно числу 2, то
+// программа выводит остаток от деления.
+// 34, 5 -> не кратно, остаток 4
+// 16, 4 -> кратно
+
+// Console.Write("Введите число 1: ");
+// double number1 = Convert.ToInt64(Console.ReadLine());
+// Console.Write("Введите число 2: ");
+// double number2 = Convert.ToInt64(Console.ReadLine());
+// int result = 0;
+
+// if (result = number1 % number2 == 0)
+// {
+//     Console.WriteLine("Кратное");
+// }
+// else
+// {
+//     Console.WriteLine("Не кратно, остаток "+result+"");
+// }
+
+//////////////////////////////////////////////////////////////////////////////////
+
+// Console.WriteLine(remainder == 0 ? "кратно" : $"не кратно, остаток {remainder}" );
+
+//////////////////////////////////////////////////////////////////////////////////
+
+Console.Clear();
+Console.Write("Введите первое число: ");
+int firstNumber = ReadConsole();
+
+Console.Write("Введите второе число: ");
+int secondNumber = ReadConsole();
+
+int result = IsMultiplicity(firstNumber, secondNumber) ;
+
+if(result == 0)
+{
+    Console.WriteLine("кратно");
+}
+else
+{
+    Console.WriteLine($"не кратно, остаток {result}");
+}
+
+int IsMultiplicity(int number1, int number2)
+{
+    return number1 % number2;
+}
+
+int ReadConsole()
+{
+    var str = Console.ReadLine();
+    int number = Convert.ToInt32(str);
+    return number;
+}
