375ef743d72623e693ecfa5b2f2dd699d8bc306d
Sergey_Puchnin <puchnin1996@mail.ru>
2023-03-10 13:30:16 +0300

    Добавили в Task010 задачу

--- Task010/Program.cs
+++ Task010/Program.cs
@@ -4,3 +4,27 @@
 // 782 -> 8
 // 918 -> 1
 
+Console.Write("Введите трёхзначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if(number >= 100 && number <= 999)
+{
+    int firstDigit = number / 100;
+    int thirdDigit = number % 10;
+    int secondDigit = (number - (firstDigit * 100 + thirdDigit))/10;
+    Console.WriteLine(secondDigit);
+}
+else
+{
+    Console.WriteLine("Введите ТРЁХзначное число: ");
+}
+
+// int finalresult = RemuveDigit(number);
+// int RemuveDigit(int num)
+// {
+//     int firstDigit = number / 100;
+//     int thirdDigit = number % 10;
+//     int secondDigit = (number - (firstDigit * 100 + thirdDigit))/10;
+//     return secondDigit;
+// }
+// Console.WriteLine(finalresult);
