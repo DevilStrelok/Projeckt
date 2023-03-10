0f433253920a7293500497299f0e2b7e0925bdad
Sergey_Puchnin <puchnin1996@mail.ru>
2023-03-10 12:59:00 +0300

    Добавили в Task016 задачу

--- Task016/Program.cs
+++ Task016/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на
+// вход два числа и проверяет, является ли одно
+// число квадратом другого.
+// 5, 25 -> да
+// -4, 16 -> да
+// 25, 5 -> да
+// 8,9 -> нет
+
+Console.WriteLine("Ведите первое число: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Ведите второе число: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+bool result = IsEqualSquared(number1, number2);
+Console.WriteLine(result ? "Да" : "Нет");
+
+bool IsEqualSquared(int num1, int num2)
+{
+    return num1 * num1 == num2 || num2 * num2 == num1;
+}
