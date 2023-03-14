8f9174f4671abb520e99952b61b920d91ff5a043
Sergey_Puchnin <puchnin1996@mail.ru>
2023-03-14 13:45:46 +0300

    Добавили в Task019 задачу

--- Task019/Program.cs
+++ Task019/Program.cs
@@ -7,3 +7,23 @@
 // 12821 -> да
 // 23432 -> да
 
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int temp = number;
+int reverse = 0;
+int digital = 0;
+
+while (number > 0)
+{
+    digital = number % 10;
+    reverse = reverse * 10 + digital;
+    number = number / 10;
+}
+if (temp == reverse)
+{
+    Console.WriteLine("Да");
+}
+else
+{
+    Console.WriteLine("Нет");
+}
