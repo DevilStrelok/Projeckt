7a279790644e2df79b57484d66db2bb8603cfd22
Sergey_Puchnin <puchnin1996@mail.ru>
2023-03-14 11:11:19 +0300

    Добавили в Task018 задачу

--- Task018/Program.cs
+++ Task018/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, 
+// которая по заданному номеру четверти, 
+// показывает диапазон возможных координат 
+// точек в этой четверти (x и y).
+
+Console.WriteLine("Ведите номер четверти: ");
+Console.Write("number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+string interval = Interval(number);
+Console.WriteLine($"Указанная четверть соответствует координатам -> {interval}");
+
+string Interval(int quarter)
+{
+    if (quarter == 1) return "x > 0 && y > 0";
+    if (quarter == 2) return "x < 0 && y > 0";
+    if (quarter == 3) return "x < 0 && y < 0";
+    if (quarter == 4) return "x > 0 && y < 0";
+    return "Такой четвери не существует";
+}
