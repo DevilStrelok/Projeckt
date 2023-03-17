9079f00660fd72578af3b6dd3c719ef7a0ea5e03
Sergey_Puchnin <puchnin1996@mail.ru>
2023-03-17 15:28:32 +0300

    Добавили в Task027 задачу

--- Task027/Program.cs
+++ Task027/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 27: 
+// Напишите программу, которая 
+// принимает на вход число и 
+// выдаёт сумму цифр в числе.
+
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int sumNumber = SumNumber(number);
+Console.WriteLine($"Сумма цифр в числе {number} -> {sumNumber}");
+
+int SumNumber(int num)
+{
+    int sum = 0;
+    while (num > 0)
+    {
+        sum = sum + num % 10;
+        num = num / 10;
+    }
+    return sum;
+}
