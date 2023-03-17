e41fe4123a03e54cdd858d24e068b215784d14e1
Sergey_Puchnin <puchnin1996@mail.ru>
2023-03-17 13:05:00 +0300

    Добавили в Task028 задачу

--- Task028/Program.cs
+++ Task028/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, 
+// которая принимает на вход число N и 
+// выдаёт произведение чисел от 1 до N.
+// 4 -> 24
+// 5 -> 120
+
+Console.WriteLine("Введите целое число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int productNumbers = ProductNumbers(number);
+Console.WriteLine($"Произведение чисел от 1 до {number} = {productNumbers}");
+
+int ProductNumbers(int num)
+{
+    int product = 1;
+    for (int count = 2; count <= num; count++)
+    {
+        checked
+        {
+            product = product * count;
+        }
+    }
+    return product;
+}
