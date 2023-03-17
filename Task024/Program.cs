8da2e0392956cd9b27f91f695b3d802520384eb0
Sergey_Puchnin <puchnin1996@mail.ru>
2023-03-17 13:04:07 +0300

    Добавили в Task024 задачу

--- Task024/Program.cs
+++ Task024/Program.cs
@@ -1,2 +1,39 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, 
+// которая принимает на вход число (А) 
+// и выдаёт сумму чисел от 1 до А.
+// 7 -> 28
+// 4 -> 10
+// 8 -> 36
+
+// Console.WriteLine("Введите целое число: ");
+// int number = Convert.ToInt32(Console.ReadLine());
+
+// int sumNumbers = SumNumbers(number);
+// Console.WriteLine($"Сумма чисел от 1 до {number} = {sumNumbers}");
+
+// int SumNumbers(int num)
+// {
+//     int sum = 0;
+//     for (int i = 1; i <= sum; i++)
+//     {
+//         sum += i; // sum = sum + i;
+//     }
+//     return sum;
+// }
+//...Не работает, не могу понять где ошибка.
+
+Console.WriteLine("Введите целое число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int sumNumbers = SumNumbers(number);
+Console.WriteLine($"Сумма чисел от 1 до {number} = {sumNumbers}");
+
+int SumNumbers(int num)
+{
+    int sum = 0;
+    for (int i = 1; i <= num; i++)
+    {
+        sum += i; // sum = sum + i;
+    }
+    return sum;
+}
