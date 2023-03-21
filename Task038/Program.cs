7828131323097cb0f1a287eda3381291efdcd32e
Sergey_Puchnin <puchnin1996@mail.ru>
2023-03-21 16:27:58 +0300

    Добавили в Task038 задачу

--- Task038/Program.cs
+++ Task038/Program.cs
@@ -4,3 +4,59 @@
 
 // [3.5, 7.1, 22.9, 2.3, 78.5] -> 76.2
 
+double[] CreateArrayRndDouble(int size, int min, int max, int round = 1)
+{
+    double[] arr = new double[size];
+    Random rnd = new Random();
+
+    for (int i = 0; i < arr.Length; i++)
+    {
+        double num = rnd.NextDouble() * (max - min) + min;
+        arr[i] = Math.Round(num, 1);
+    }
+    return arr;
+}
+
+void PrintArray(double[] arr)
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i < arr.Length - 1) Console.Write($"{arr[i]}; ");
+        else Console.Write($"{arr[i]}");
+    }
+    Console.Write("]");
+}
+
+double MaxArray(double[] arr)
+{
+    double max = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] > max) max = arr[i];
+    }
+    return max;
+}
+
+double MinArray(double[] arr)
+{
+    double min = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] < min) min = arr[i];
+    }
+    return min;
+}
+
+double DifferenceArray(double[] arr)
+{
+    double max = MaxArray(arr);
+    double min = MinArray(arr);
+    double difference = max - min;
+    return difference;
+}
+
+double[] array = CreateArrayRndDouble(10, -10, 10);
+PrintArray(array);
+double difference = DifferenceArray(array);
+Console.WriteLine($" -> {difference}");
