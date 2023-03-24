4eef88af0d310907f4ae9244688cc16299c8be85
Sergey_Puchnin <puchnin1996@mail.ru>
2023-03-24 12:47:02 +0300

    Добавили в Task040 задачу

--- Task040/Program.cs
+++ Task040/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 40: 
+// Напишите программу, 
+// которая принимает на вход три числа и проверяет, 
+// может ли существовать треугольник со сторонами такой длины.
+// Теорема о неравенстве треугольника: 
+// каждая сторона треугольника меньше суммы двух других сторон.
+
+Console.Clear();
+
+Console.Write("Введите длину строны 1: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите длину строны 2: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите длину строны 3: ");
+int number3 = Convert.ToInt32(Console.ReadLine());
+
+bool result = IsExistTriangle(number1, number2, number3);
+Console.WriteLine(result ? "Существует" : "Не существует");
+
+bool IsExistTriangle(int n1, int n2, int n3)
+{
+    return n1 < n2 + n3 && n2 < n1 + n3 && n3 < n1 + n2;
+}
