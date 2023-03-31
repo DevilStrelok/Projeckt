f93424010a9074a9fb1d19a9687c2cf6a00fd475
Sergey_Puchnin <puchnin1996@mail.ru>
2023-03-31 13:45:38 +0300

    Добавили в Task055 задачу

--- Task055/Program.cs
+++ Task055/Program.cs
@@ -1,2 +1,62 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 55: 
+// Задайте двумерный массив. 
+// Напишите программу, которая заменяет строки на столбцы. 
+// В случае, если это невозможно, программа должна вывести сообщение для пользователя.
+
+int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
+{
+    int[,] matrix = new int[rows, columns];
+    Random rnd = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("|");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j],4} ");
+        }
+        Console.WriteLine("|");
+    }
+}
+
+bool SquareMatrix(int[,] matrix)
+{
+    return (matrix.GetLength(0) == matrix.GetLength(1));
+}
+
+void ChangeRowsColumnsMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < i; j++)
+        {
+            int temp = matrix[i, j];
+            matrix[i, j] = matrix[j, i];
+            matrix[j, i] = temp;
+        }
+    }
+}
+
+int[,] array2d = CreateMatrixRndInt(5, 5, 0, 10);
+PrintMatrix(array2d);
+
+Console.WriteLine();
+
+if (SquareMatrix(array2d))
+{
+    ChangeRowsColumnsMatrix(array2d);
+    PrintMatrix(array2d);
+}
+else Console.WriteLine("Ошибка. Число строк не равно числу столбцов.");
