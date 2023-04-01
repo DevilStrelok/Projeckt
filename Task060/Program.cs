1ad7061180b0d28f9e5983ac25cb2776a6a71faf
Sergey_Puchnin <puchnin1996@mail.ru>
2023-04-01 10:49:31 +0300

    Добавили в Task060 решение задачи

--- Task060/Program.cs
+++ Task060/Program.cs
@@ -9,3 +9,40 @@
 // 66(0,0,0) 27(0,0,1) 25(0,1,0) 90(0,1,1)
 // 34(1,0,0) 26(1,0,1) 41(1,1,0) 55(1,1,1)
 
+int[,,] CreateMatrixRndInt3D(int rows, int columns, int depth, int min, int max)
+{
+    int[,,] matrix = new int[rows, columns, depth];
+    Random rnd = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            for (int k = 0; k < matrix.GetLength(2); k++)
+            {
+                matrix[i, j, k] = rnd.Next(min, max + 1);
+            }
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix3D(int[,,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"X({i}) Y({j}) ");
+            for (int k = 0; k < matrix.GetLength(2); k++)
+            {
+                Console.Write($"Z({k}) = |{matrix[i, j, k],2}|; ");
+            }
+            Console.WriteLine();
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,,] array3d = CreateMatrixRndInt3D(4, 4, 4, 0, 10);
+PrintMatrix3D(array3d);
