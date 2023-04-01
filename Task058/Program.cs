08f2e5802b010ef7d9b6830957d0da2595f345f2
Sergey_Puchnin <puchnin1996@mail.ru>
2023-04-01 10:49:06 +0300

    Добавили в Task058 решение задачи

--- Task058/Program.cs
+++ Task058/Program.cs
@@ -10,3 +10,72 @@
 // 18 20
 // 15 18
 
+int[,] CreateMatrixRndIntFirst(int rows, int columns, int min, int max)
+{
+    int[,] matrix = new int[rows, columns];
+    Random rnd = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return matrix;
+}
+
+int[,] CreateMatrixRndIntSecond(int rows, int columns, int min, int max)
+{
+    int[,] matrix = new int[rows, columns];
+    Random rnd = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("|");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j],4} ");
+        }
+        Console.WriteLine("|");
+    }
+    Console.WriteLine();
+}
+
+void MultiplyMatrix(int[,] matrixFirst, int[,] matrixSecond, int[,] matrixResult)
+{
+    for (int i = 0; i < matrixResult.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrixResult.GetLength(1); j++)
+        {
+            int sum = 0;
+            for (int k = 0; k < matrixFirst.GetLength(1); k++)
+            {
+                sum += matrixFirst[i, k] * matrixSecond[k, j];
+            }
+            matrixResult[i, j] = sum;
+        }
+    }
+}
+
+int[,] matrixFirst = CreateMatrixRndIntFirst(4, 4, 0, 10);
+PrintMatrix(matrixFirst);
+
+int[,] matrixSecond = CreateMatrixRndIntSecond(4, 4, 0, 10);
+PrintMatrix(matrixSecond);
+
+int[,] matrixResult = new int[4, 4];
+MultiplyMatrix(matrixFirst, matrixSecond, matrixResult);
+PrintMatrix(matrixResult);
