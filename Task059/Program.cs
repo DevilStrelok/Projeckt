c460b81007b3fc0a21407016c23ea9b33674b2bd
Sergey_Puchnin <puchnin1996@mail.ru>
2023-04-01 10:48:12 +0300

    Добавили в Task059 решение задачи

--- Task059/Program.cs
+++ Task059/Program.cs
@@ -32,5 +32,56 @@ void PrintMatrix(int[,] matrix)
     Console.WriteLine();
 }
 
-int[,] array2d = CreateMatrixRndInt(5, 5, -10, 10);
+int[,] FindPositionSmallElement(int[,] matrix, int[,] position)
+{
+    int temp = matrix[0, 0];
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (matrix[i, j] <= temp)
+            {
+                position[0, 0] = i;
+                position[0, 1] = j;
+                temp = matrix[i, j];
+            }
+        }
+    }
+    Console.WriteLine($"Mинимальный элемент: {temp}");
+    return position;
+}
+
+void DeleteLines(int[,] matrix, int[,] positionSmallElement, int[,] arrayWithoutLines)
+{
+    int k = 0, l = 0;
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (positionSmallElement[0, 0] != i && positionSmallElement[0, 1] != j)
+            {
+                arrayWithoutLines[k, l] = matrix[i, j];
+                l++;
+            }
+        }
+        l = 0;
+        if (positionSmallElement[0, 0] != i)
+        {
+            k++;
+        }
+    }
+}
+
+int[,] array2d = CreateMatrixRndInt(4, 4, 0, 10);
 PrintMatrix(array2d);
+
+int[,] positionSmallElement = new int[1, 2];
+positionSmallElement = FindPositionSmallElement(array2d, positionSmallElement);
+Console.Write($"Позиция элемента: ");
+Console.WriteLine();
+PrintMatrix(positionSmallElement);
+
+int[,] arrayWithoutLines = new int[array2d.GetLength(0) - 1, array2d.GetLength(1) - 1];
+DeleteLines(array2d, positionSmallElement, arrayWithoutLines);
+Console.WriteLine($"Получившийся массив:");
+PrintMatrix(arrayWithoutLines);
