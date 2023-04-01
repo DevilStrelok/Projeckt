15c298dfc4bd6bc16364dbdc55f557d30135ae46
Sergey_Puchnin <puchnin1996@mail.ru>
2023-04-01 10:49:51 +0300

    Добавили в Task062 решение задачи

--- Task062/Program.cs
+++ Task062/Program.cs
@@ -8,3 +8,51 @@
 // 11 16 15 06
 // 10 09 08 07
 
+int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
+{
+    int[,] matrix = new int[rows, columns];
+    Random rnd = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return matrix;
+}
+
+void FillMatrixSpiral(int[,] matrix)
+{
+    int temp = 1;
+    int i = 0;
+    int j = 0;
+    while (temp <= matrix.GetLength(0) * matrix.GetLength(1))
+    {
+        matrix[i, j] = temp;
+        temp++;
+        if (i <= j + 1 && i + j < matrix.GetLength(1) - 1) j++;
+        else if (i < j && i + j >= matrix.GetLength(0) - 1) i++;
+        else if (i >= j && i + j > matrix.GetLength(1) - 1) j--;
+        else i--;
+    }
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("|");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j],4} ");
+        }
+        Console.WriteLine("|");
+    }
+    Console.WriteLine();
+}
+
+int[,] array2d = CreateMatrixRndInt(4, 4, 0, 10);
+FillMatrixSpiral(array2d);
+PrintMatrix(array2d);
