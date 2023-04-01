b076d81f7da5277880ea1c0850bb90ffcec16f31
Sergey_Puchnin <puchnin1996@mail.ru>
2023-04-01 10:48:30 +0300

    Добавили в Task054 решение задачи

--- Task054/Program.cs
+++ Task054/Program.cs
@@ -12,3 +12,58 @@
 // 9 5 3 2
 // 8 4 4 2
 
+int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
+{
+    int[,] matrix = new int[rows, columns];
+    Random rnd = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("|");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j],4} ");
+        }
+        Console.WriteLine("|");
+    }
+    Console.WriteLine();
+}
+
+void OrderArrayLines(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            for (int k = 0; k < matrix.GetLength(1) - 1; k++)
+            {
+                if (matrix[i, k] < matrix[i, k + 1])
+                {
+                    int temp = matrix[i, k + 1];
+                    matrix[i, k + 1] = matrix[i, k];
+                    matrix[i, k] = temp;
+                }
+            }
+        }
+    }
+}
+
+int[,] array2d = CreateMatrixRndInt(4, 4, 0, 10);
+PrintMatrix(array2d);
+
+Console.WriteLine("Сортировка массива\n");
+
+OrderArrayLines(array2d);
+PrintMatrix(array2d);
