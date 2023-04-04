deff6218dc0481471d4bc0580459f0e0bc2dabac
Sergey_Puchnin <puchnin1996@mail.ru>
2023-04-04 12:51:38 +0300

    Добавили Task069 с задачей

---
+++ Task069/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 69: 
+// Напишите программу, 
+// которая на вход принимает два числа A и B, 
+// и возводит число А в целую степень B с помощью рекурсии.
+// A = 3; B = 5 -> 243 (3⁵)
+// A = 2; B = 3 -> 8
+
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите степень: ");
+int degree = Convert.ToInt32(Console.ReadLine());
+
+int degreeNumber = DegreeNumder(number, degree);
+Console.WriteLine($"Число {number} в степени {degree} -> {degreeNumber}");
+
+int DegreeNumder(int num, int deg)
+{
+    if (deg == 0) return 1;
+    else return num * DegreeNumder(num, deg - 1);
+}
