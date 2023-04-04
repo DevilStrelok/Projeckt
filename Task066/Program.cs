e901c6cf12940af03b65de89d66c4a0c43959a9e
Sergey_Puchnin <puchnin1996@mail.ru>
2023-04-04 14:06:42 +0300

    Добавили в Task066 решение задачи

--- Task066/Program.cs
+++ Task066/Program.cs
@@ -6,3 +6,24 @@
 // M = 1; N = 15 -> 120
 // M = 4; N = 8. -> 30
 
+Console.WriteLine($"Введите число M: ");
+int numberM = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($"Введите число N: ");
+int numberN = Convert.ToInt32(Console.ReadLine());
+
+int nuturalNumberSum = NuturalNumberSum(numberM, numberN);
+Console.WriteLine(nuturalNumberSum);
+
+int NuturalNumberSum(int numM, int numN)
+{
+    if (numM < numN)
+    {
+        return numM + NuturalNumberSum(numM + 1, numN);
+    }
+    else if (numM > numN)
+    {
+        return numM + NuturalNumberSum(numM - 1, numN);
+    }
+    else return numM;
+}
