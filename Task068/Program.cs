1c3295c3a6e5c996e6d7c8716a755ceea93a5215
Sergey_Puchnin <puchnin1996@mail.ru>
2023-04-04 14:06:59 +0300

    Добавили в Task068 решение задачи

--- Task068/Program.cs
+++ Task068/Program.cs
@@ -4,3 +4,18 @@
 // m = 2, n = 3 -> A(m,n) = 9
 // m = 3, n = 2 -> A(m,n) = 29
 
+Console.WriteLine($"Введите число M: ");
+int numberM = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($"Введите число N: ");
+int numberN = Convert.ToInt32(Console.ReadLine());
+
+int ackermanfunction = AckermanFunction(numberM, numberN);
+Console.WriteLine(ackermanfunction);
+
+int AckermanFunction(int numM, int numN)
+{
+    if (numM == 0) return numN + 1;
+    else if (numN == 0) return AckermanFunction(numM - 1, 1);
+    return AckermanFunction(numM - 1, AckermanFunction(numM, numN - 1));
+}
